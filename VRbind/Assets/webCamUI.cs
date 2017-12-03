90d1282afc1540cfd81d9505336d7f8e3ab4e7e6
Meenakshi Sivapriya <meenakshisivapriya@gmail.com>
2017-12-02 21:39:50 -0500

    Unity with tts and subs

---
+++ VRbind/Assets/webCamUI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+ using System.Text;
+ using System.IO; 
+
+public class webCamUI : MonoBehaviour {
+
+     public RawImage rawimage;
+     private string text;
+     public Text subtitlesText;
+     void Start () 
+     {
+     	subtitlesText = subtitlesText.GetComponent<Text>();
+
+         WebCamTexture webcamTexture = new WebCamTexture();
+         rawimage.texture = webcamTexture;
+         rawimage.material.mainTexture = webcamTexture;
+         webcamTexture.Play();
+
+         
+     }
+
+     void Update()
+     {
+     	text = System.IO.File.ReadAllText("Assets/Resources/subtitles.txt");
+        Debug.Log(text);
+     	
+     	if(text[0].Equals('*')){
+     		subtitlesText.text = text;
+     		WindowsVoice.theVoice.speak(text);
+
+     		}
+     	else{
+     			Debug.Log("Passed");
+     		}
+     }
+
+}
